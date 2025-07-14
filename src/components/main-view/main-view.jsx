7123ce659b921670131a6e2be0b30fba95efc815
JDSwan11995 <jdswan1995@gmail.com>
2025-07-14 16:18:29 -0500

    exercise 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,52 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "My Neighbor Totoro",
-      image: "/images/totoro.jpg",
-      director: "Hayao Miyazaki",
-      description:
-        "Young sisters, Satsuki and Mei, move to the countryside with their father to be near their ailing mother, they soon meet the friendly and curious spirits of the forest.",
-      genre: "Fantasy",
-      release: "April 16 1988",
-      rating: "94%",
-    },
-    {
-      id: 2,
-      title: "Grave of the Fireflies",
-      image: "/images/fireflies.jpg",
-      director: "Isao Takahata",
-      description:
-        "A poignant animated film about two siblings struggling to survive in wartime Japan.",
-      genre: "Period Drama",
-      release: "April 16 1988",
-      rating: "100%",
-    },
-    {
-      id: 3,
-      title: "The Red Turtle",
-      image: "/images/turtle.jpg",
-      director: "Michaël Dudok de Wit",
-      description:
-        "A wordless meditative tale about a man's life on a deserted island and the profound connection he forms with nature.",
-      genre: "Drama",
-      release: "May 18 2016",
-      rating: "93%",
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://ghibliheroku-f28bf5d9329a.herokuapp.com/movies")
+      .then((response) => {
+        console.log("Raw response from Server: ", response);
+        if (!response.ok) {
+          throw new Error("Network Response was not okay");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Fetched movies data: ", data[0]);
+        setMovies(data);
+      })
+      .catch((error) => {
+        console.error("Error catching movies: ", error.message);
+      });
+  }, []);
 
   console.log(
     "MainView - movies:",
-    movies.map((m) => ({
-      title: m.title,
-      image: m.image,
-    }))
+    movies.map((movie) => <MovieCard key={movie._id} movie={movie} />)
   );
 
   if (selectedMovie) {
@@ -66,7 +46,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie._id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
