53a13105bcc368efb78a4531aa5851405bd01cb8
JDSwan11995 <jdswan1995@gmail.com>
2025-07-21 15:39:55 -0500

    Exercise 3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,53 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "My Neighbor Totoro",
-      image: "../images/totoro.jpg",
-      director: "Hayao Miyazaki",
-      description:
-        "Young sisters, Satsuki and Mei, move to the countryside with their father to be near their ailing mother, they soon meet the friendly and curious spirits of the forest.",
-      genre: "Fantasy",
-      release: "April 16 1988",
-      rating: "94%",
-    },
-    {
-      id: 2,
-      title: "Grave of the Fireflies",
-      image: "../images/fireflies.jpg",
-      director: "Isao Takahata",
-      description:
-        "A poignant animated film about two siblings struggling to survive in wartime Japan.",
-      genre: "Period Drama",
-      release: "April 16 1988",
-      rating: "100%",
-    },
-    {
-      id: 3,
-      title: "The Red Turtle",
-      image: "../images/turtle.jpg",
-      director: "Michaël Dudok de Wit",
-      description:
-        "A wordless meditative tale about a man's life on a deserted island and the profound connection he forms with nature.",
-      genre: "Drama",
-      release: "May 18 2016",
-      rating: "93%",
-    },
-  ]);
-
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  console.log(
-    "MainView - movies:",
-    movies.map((m) => ({
-      title: m.title,
-      image: m.image,
-    }))
-  );
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
+
+  useEffect(() => {
+    if (!token) return;
+    fetch("https://ghibliheroku-f28bf5d9329a.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, [token]);
+
+  console.log("MainView - movies:", movies);
 
   if (selectedMovie) {
     return (
@@ -59,14 +50,36 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+        <div>The list is empty!</div>
+      </>
+    );
   }
 
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      >
+        Logout
+      </button>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie._id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
